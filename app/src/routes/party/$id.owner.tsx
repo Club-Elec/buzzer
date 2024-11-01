8999e3154ccce1417ed4e97abaebe87ee11f5de6
Romain <legall.romain29470@gmail.com>
2024-11-01 13:44:44 +0100

    🔧 fix: owner responsivness

--- app/src/routes/party/$id.owner.tsx
+++ app/src/routes/party/$id.owner.tsx
@@ -49,35 +49,45 @@ const PartyOwner = () => {
 
   return (
     <div className="w-full h-full flex items-center justify-center p-4">
-      <div className="h-3/4 flex flex-col items-center justify-between gap-4">
+      <div className="h-full flex flex-col items-center justify-between gap-4">
         <h1 className="text-5xl text-red-950 font-bold">Buzz Ta Mère</h1>
 
-        <div className="w-96 flex flex-col items-center gap-4">
-          {party && party.players.length > 0 ? (
-            party.players.map((name) => {
-              const hasSubmitted = party.submitted.indexOf(name);
+        <div className="relative w-[90%] h-full flex flex-col overflow-y-hidden">
+          <div className="absolute -top-1 left-0 w-full h-8 bg-gradient-to-b from-white to-transparent" />
 
-              return (
-                <div
-                  key={name}
-                  className="w-full h-16 flex items-center justify-between px-4 py-2 border border-red-400 bg-red-100 rounded-2xl shadow-xl shadow-red-200 text-lg font-semibold"
-                >
-                  <p>{name}</p>
+          <div className="h-full w-full overflow-y-auto flex flex-col items-center gap-4">
+            <div className="h-4" />
 
-                  <div className="h-full aspect-square flex items-center justify-center font-bold">
-                    {hasSubmitted === -1 ? "-" : hasSubmitted + 1}
+            {party && party.players.length > 0 ? (
+              party.players.map((name) => {
+                const hasSubmitted = party.submitted.indexOf(name);
+
+                return (
+                  <div
+                    key={name}
+                    className="w-full h-16 flex items-center justify-between px-4 py-2 border border-red-400 bg-red-100 rounded-2xl shadow-xl shadow-red-200 text-lg font-semibold"
+                  >
+                    <p>{name}</p>
+
+                    <div className="h-full aspect-square flex items-center justify-center font-bold">
+                      {hasSubmitted === -1 ? "-" : hasSubmitted + 1}
+                    </div>
                   </div>
-                </div>
-              );
-            })
-          ) : (
-            <div className="w-full h-16 flex items-center justify-between px-4 py-2 border border-red-400 bg-red-100 rounded-2xl shadow-xl shadow-red-200 text-lg font-semibold">
-              <p>Aucun joueur...</p>
-            </div>
-          )}
+                );
+              })
+            ) : (
+              <div className="w-full h-16 flex items-center justify-between px-4 py-2 border border-red-400 bg-red-100 rounded-2xl shadow-xl shadow-red-200 text-lg font-semibold">
+                <p>Aucun joueur...</p>
+              </div>
+            )}
+
+            <div className="h-4" />
+          </div>
+
+          <div className="absolute -bottom-1 left-0 w-full h-8 bg-gradient-to-t from-white to-transparent" />
         </div>
 
-        <div className="flex flex-col items-center gap-4">
+        <div className="w-[90%] flex flex-col items-center gap-4">
           <button
             className="w-full h-16 flex items-center justify-between px-4 py-2 border border-red-400 bg-red-100 rounded-2xl shadow-xl shadow-red-200 text-lg font-semibold active:scale-95 transition ease-in-out duration-300"
             onClick={onReset}
